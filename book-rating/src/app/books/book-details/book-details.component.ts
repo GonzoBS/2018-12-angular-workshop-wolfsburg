419351431516054ded75bacc3b313a5bb96b3140
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-12-07 09:59:13 +0100

    the worst RxJS anti-pattern of all times

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BookStoreService } from '../shared/book-store.service';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-book-details',
@@ -10,13 +11,21 @@ import { BookStoreService } from '../shared/book-store.service';
 export class BookDetailsComponent implements OnInit {
 
   isbn: string;
+  book: Book;
 
   constructor(private route: ActivatedRoute, private service: BookStoreService) { }
 
   ngOnInit() {
 
     this.route.paramMap
-      .subscribe(paramMap => this.isbn = paramMap.get('isbn'));
+      .subscribe(paramMap => {
+
+        this.isbn = paramMap.get('isbn');
+
+        // the worst RxJS anti-pattern of all times!
+        this.service.getSingle(this.isbn)
+          .subscribe(book => this.book = book);
+      });
   }
 
 }
