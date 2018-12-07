f4588d46bfb568a12ea61fe785170f5c4876f6f6
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-12-07 11:14:10 +0100

    mergeMap

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BookStoreService } from '../shared/book-store.service';
 import { Book } from '../shared/book';
+import { of, from, Observable } from 'rxjs';
+import { map, filter, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'br-book-details',
@@ -17,6 +19,40 @@ export class BookDetailsComponent implements OnInit {
 
   ngOnInit() {
 
+    /*
+    // Observer
+    const observer = {
+      next: n => console.log(n),
+      error: e => console.log('ERROR', e),
+      complete: () => console.log('COMPLETE')
+    };
+
+    // Observable
+    const myObservable$ = new Observable<number>(obs => {
+
+      obs.next(2);
+      setTimeout(() => obs.next(5), 1000);
+      setTimeout(() => obs.next(6), 1000);
+      // setTimeout(() => obs.error(-1), 2000);
+      setTimeout(() => obs.complete(), 2000);
+
+    });
+
+    // import { map } from 'rxjs/operators';
+    // Subscription
+    const subscription = myObservable$
+      .pipe(
+        map(number => number * 10),
+        filter(number => number > 20),
+        map(number => 'TEST ' + number)
+      )
+      .subscribe(observer);
+
+    setTimeout(() => subscription.unsubscribe(), 3000);
+
+    */
+
+    /*
     this.route.paramMap
       .subscribe(paramMap => {
 
@@ -26,6 +62,13 @@ export class BookDetailsComponent implements OnInit {
         this.service.getSingle(this.isbn)
           .subscribe(book => this.book = book);
       });
+    */
+
+    this.route.paramMap.pipe(
+      map(params => params.get('isbn')),
+      mergeMap(isbn => this.service.getSingle(isbn))
+    )
+    .subscribe(book => this.book = book);
   }
 
 }
